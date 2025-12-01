032a7b2765710cd2a80b2aab4beb515bd8cf1ba9
Ilies BELLAL <iliesbellal@gmail.com>
2025-12-01 23:52:57 +0100

    adding delivery sessions

--- internal/repositories/delivery_sessions_repo.go
+++ internal/repositories/delivery_sessions_repo.go
@@ -520,7 +520,7 @@ func (r *DeliverySessionsRepository) GetDeliverySession(ctx context.Context, mer
 
 	for _, oid := range orderIDs {
 		filter := fmt.Sprintf(" AND o.order_id = '%s' ", oid)
-		orders, err := r.ordersFetcher.fetchAndBuildOrders(context.Background(), merchantID, filter)
+		orders, err := r.ordersFetcher.fetchAndBuildOrders(ctx, merchantID, filter)
 		if err != nil {
 			return nil, err
 		}
