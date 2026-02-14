eda8a976a835f334a1890484d788c4ee5083da9c
Ilies BELLAL <iliesbellal@gmail.com>
2026-02-14 12:59:33 +0100

    fixing webhook deliveroo for test scenarios

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -9,6 +9,9 @@ func CORSMiddleware() *cors.Cors {
 			"https://mywelloresto.welloresto.fr",
 			"https://wello-back-office.onrender.com",
 
+			// Deliveroo
+			"https://api.developers.deliveroo.com",
+
 			// Lovable
 			"https://d97a501b-ebd7-46a4-9288-bca03030e30f.lovableproject.com",
 
