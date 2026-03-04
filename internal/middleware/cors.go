a46b27f6d0ed3fa2c034efb597043eb3328ad392
Ilies BELLAL <iliesbellal@gmail.com>
2026-03-04 21:07:06 +0100

    lovable

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -11,6 +11,7 @@ func CORSMiddleware() *cors.Cors {
 
 			// Lovable
 			"*.lovableproject.com",
+			"https://id-preview--4cbec765-6bdb-4c2c-8fe8-bbe5ee8d316a.lovable.app",
 
 			// Postman
 			"https://wello-resto.postman.co",
