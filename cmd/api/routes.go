66ba0405b5ab5ce1489c963cc266a8ecbf097a69
Ilies BELLAL <iliesbellal@gmail.com>
2026-04-06 03:42:54 +0200

    updates backoffice

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -433,7 +433,7 @@ func SetupRoutes(log *zap.Logger, mysqlDB *sql.DB, cfg *config.AppConfig) *chi.M
 		r.Patch("/component/{component_id}/status", menuH.SetComponentStatus)
 		r.Delete("/components/{component_id}", menuH.DeleteComponent)
 
-		r.Patch("/display-order", menuH.UpdateDisplayOrder)
+		r.Patch("/display-orders", menuH.UpdateDisplayOrder)
 
 		r.Patch("/products/category/{category_id}", menuH.UpdateProductCategory)
 		r.Patch("/products/category/{category_id}/availability", menuH.SetProductCategoryAvailability)
