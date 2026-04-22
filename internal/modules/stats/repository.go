ed2c60604c22f20681c424139ced091d7d588f2e
Ilies BELLAL <iliesbellal@gmail.com>
2026-04-22 21:50:05 +0200

    feature: stats from db

--- internal/modules/stats/repository.go
+++ internal/modules/stats/repository.go
@@ -155,7 +155,7 @@ func (r *StatsRepository) GetAverageBasket(ctx context.Context, merchantID strin
 // getAverageBasketForPeriod calculates average basket size for a period
 func (r *StatsRepository) getAverageBasketForPeriod(ctx context.Context, merchantID string, startTime, endTime time.Time) (int64, error) {
 	query := `
-	SELECT COALESCE(AVG(o.price), 0) as avg_basket
+	SELECT ROUND(COALESCE(AVG(o.price), 0),0) as avg_basket
 	FROM orders o
 	WHERE o.merchant_id = ?
 	AND o.creation_date >= ?
