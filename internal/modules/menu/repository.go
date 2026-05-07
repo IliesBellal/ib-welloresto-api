0ea597806fe1be59b6282e74ad5f1b03be60449c
Ilies BELLAL <iliesbellal@gmail.com>
2026-05-07 17:03:16 +0200

    feature: menu with marketing categories

--- internal/modules/menu/repository.go
+++ internal/modules/menu/repository.go
@@ -616,7 +616,8 @@ func (r *MenuRepository) GetMenu(ctx context.Context, merchantID string, lastMen
             INNER JOIN tva_categories tva_delivery on tva_delivery.tva_id = p.tva_delivery_id
             INNER JOIN tva_categories tva_take_away on tva_take_away.tva_id = p.tva_take_away_id
             LEFT JOIN products subp on subp.product_id = p.by_product_of
-            WHERE p.merchant_id = ? AND (subp.product_id IS NULL OR subp.product_id = p.product_id) AND p.available = 1 AND p.enabled = 1
+            WHERE p.merchant_id = ?
+			AND (subp.product_id IS NULL OR subp.product_id = p.product_id) AND p.status in ("1","available") AND p.enabled = 1
         `
 		rows, err := runQuery(step, q, merchantID)
 		if err != nil {
@@ -699,7 +700,7 @@ func (r *MenuRepository) GetMenu(ctx context.Context, merchantID string, lastMen
             INNER JOIN tva_categories tva_in on tva_in.tva_id = p.tva_in_id
             INNER JOIN tva_categories tva_delivery on tva_delivery.tva_id = p.tva_delivery_id
             INNER JOIN tva_categories tva_take_away on tva_take_away.tva_id = p.tva_take_away_id
-            WHERE p.merchant_id = ? AND p.by_product_of IS NOT NULL AND p.available = 1
+            WHERE p.merchant_id = ? AND p.by_product_of IS NOT NULL AND p.status in ("1","available") AND p.enabled = 1
         `
 		rows, err := runQuery(step, q, merchantID)
 		if err != nil {
